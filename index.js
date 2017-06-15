f6bffc2e3fcf066009caa3fd31118cd5909d2104
skaushik2 <shashank_kaushik@intuit.com>
2017-06-15 21:13:04 +0530

    index addition

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000; 
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.get("/webhook", function (req, res) {
+  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port: '+ port)
+});
