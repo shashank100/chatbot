6e8c124ecd848db0636ea089b111b19584a6198a
skaushik2 <shashank_kaushik@intuit.com>
2017-06-15 21:58:14 +0530

    pushing code

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var app = express();
-var port = process.env.PORT || 3000; 
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
 app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+  if (req.query["hub.verify_token"] === "my-chat-bot") {
+  //if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
     res.status(200).send(req.query["hub.challenge"]);
   } else {
     res.sendStatus(403);
