5b47860f5a81ba12eaf195eca766bb3fd815197e
skaushik2 <shashank_kaushik@intuit.com>
2017-06-17 19:16:34 +0530

    FB integration

--- index.js
+++ index.js
@@ -3,10 +3,42 @@ var app = express();
 var port = process.env.PORT || 3000;
 var superagent = require("superagent");
 var bodyParser = require("body-parser");
+var sign = "";
+var date = [];
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+function sendHoroscope(event, horoscope) {
+    superagent
+        .post("https://graph.facebook.com/v2.6/me/messages?access_token=" + process.env.PAGE_ACCESS_TOKEN)
+        .send({
+          recipient: {id: event.sender.id},
+          message: message,
+        })
+        .end(function(err, res) {
+            if(err) {
+                console.log("Error sending message: " + response.error);
+            }
+        });
+}
+
+function getHoroscope(event) {
+    console.log(sign)
+    var horoscopeDate = "today";
+    var dateToday = new Date();
+    if(date > dateToday.getUTCDate()) {
+        horoscopeDate = "tomorrow"
+    } else if (date > dateToday.getUTCDate()) {
+        horoscopeDate = "yesterday"
+    }
+    superagent
+        .get("http://http://sandipbgt.com/theastrologer/api/horoscope/" + sign + "/" + horoscopeDate)
+        .end(function(err, res) {
+            sendHoroscope(event, res.body.horoscope);
+        }.bind(this));
+}
 
 function processMessage(event) {
     var senderId = event.sender.id,
@@ -16,9 +48,11 @@ function processMessage(event) {
         .get("https://api.api.ai/api/query?v=20150910&lang=en&sessionId=mySession&query=" + payload)
         .set("Authorization", "Bearer " + process.env.API_CLIENT_TOKEN)
         .end(function(err, res) {
-            console.log(res.body);
-            //console.log(res.body.result.parameters.SunSigns);
-        });
+            sign = res.body.result.parameters.SunSigns;
+            date = Number(res.body.result.parameters.SunSigns.split("-")[2]);
+
+            getHoroscope(event);
+        }.bind(this));
 }
 
 app.get("/webhook", function (req, res) {
