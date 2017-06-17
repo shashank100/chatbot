8f20e1e015bc6491dbef37086373bf72c7fa8700
skaushik2 <shashank_kaushik@intuit.com>
2017-06-17 18:07:01 +0530

    FB integration

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ function processMessage(event) {
 
 app.post("/webhook", function (req, res) {
   // checking if it is a page subscription
+  console.log(req)
   if (req.body.object === "page") {
     // Iterate over each entry
     // There may be multiple batched entries
